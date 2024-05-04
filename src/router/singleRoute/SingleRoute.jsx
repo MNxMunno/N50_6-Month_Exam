52fb54fd7310747f009ce503d8db722638b622b1
MNxMunno <bilolhuzayfa@gmmail.com>
2024-05-04 12:17:10 +0500

    change

--- src/router/singleRoute/SingleRoute.jsx
+++ src/router/singleRoute/SingleRoute.jsx
@@ -3,6 +3,9 @@ import { useParams } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
 import Banner from "../../static/banner/Banner";
 import Loading from "../../components/loading/Loading";
+import { FaCartShopping } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa";
+import { PiArrowBendDownRightBold } from "react-icons/pi";
 
 const SingleRoute = () => {
   const { id } = useParams();
@@ -11,6 +14,13 @@ const SingleRoute = () => {
   if (error) {
     return <h2 style={{ paddingTop: "90px" }}>Mahsulot topilmadi</h2>;
   }
+  let [count, setCount] = useState(0);
+  const inc = () => {
+    setCount(count + 1);
+  };
+  const dec = () => {
+    setCount(count - 1);
+  };
 
   return (
     <section className="singleRoute">
@@ -19,9 +29,9 @@ const SingleRoute = () => {
           <div className="content__card">
             <div className="cards">
               {loading ? <Loading /> : <></>}
-              <div className="card">
+              <div className="card cardd">
                 <img
-                  className="hero"
+                  className="hero___card"
                   src={data?.data.image}
                   width={350}
                   height={250}
@@ -49,6 +59,27 @@ const SingleRoute = () => {
                   ${data?.data.price} <del>{data?.data.price * 2}</del>{" "}
                 </b>
                 <p className="desc">{data?.data.description}</p>
+                <div className="card__btns">
+                  <div className="count">
+                    <button onClick={dec} disabled={count <= 1}>
+                      -
+                    </button>
+                    <h1>{count}</h1>
+                    <button onClick={inc} disabled={count >= 10}>
+                      +
+                    </button>
+                  </div>
+                  <button className="add">
+                    <FaCartShopping />
+                    <p>Add to cart</p>
+                  </button>
+                  <button className="like">
+                    <FaRegHeart />
+                  </button>
+                  <button className="arrow">
+                    <PiArrowBendDownRightBold />
+                  </button>
+                </div>
               </div>
             </div>
           </div>
